5e7fe0bcc847b179b819d27d879e48394021a10d
AyyJimmehh <62141823+AyyJimmehh@users.noreply.github.com>
2020-04-26 19:16:47 +0300

    Add files via upload

---
+++ src/cmake-build-debug/CMakeFiles/HW.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW.dir/Source.cpp.obj"
+  "HW.exe"
+  "HW.exe.manifest"
+  "HW.lib"
+  "HW.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
